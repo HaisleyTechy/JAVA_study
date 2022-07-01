3b5a07b773a546acffa2ce7ece8be62e12ce0867
GeonwonSeo <pisatan@gmail.com>
2022-07-01 09:40:50 +0900

    '정수타입'

--- src/JAVA/Main.java
+++ src/JAVA/Main.java
@@ -19,11 +19,16 @@ public class Main {
         final long LIGHT_YEAR = 9460730472580L;
         float thing = 9460730472580.0F; //Float type 리터럴뒤F
         double thing2 = 9460730472580.0D; //double type 리터럴뒤D
-        byte byteNum = 123;
+        byte byteNum = 125;
+        byte byteNum2 = -125;
         short shortNum = 12345;
         int intNum = 123456789;
         long longNum = 12345678910L; //long타입 리터럴뒤L
         long longNum2 = 12_345_678_910L; //길면 언더바로 구분가능
+        byteNum = (byte) (byteNum + 10);
+        byteNum2 = (byte) (byteNum2 - 10);
+        System.out.println(byteNum); //오버플로우
+        System.out.println((byteNum2)); //언더플로우
     }
 }
 
