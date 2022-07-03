91c71b9f92dffa040c1415559c5f158f30cbd17f
GeonwonSeo <pisatan@gmail.com>
2022-07-04 04:21:51 +0900

    fizzBuzz

--- src/Section_1/Practice.java
+++ src/Section_1/Practice.java
@@ -1,5 +1,5 @@
 package Section_1;
-
+import java.util.StringTokenizer;
 public class Practice {
     public int getRunCatDistance(int speed, int time) {
         int distance;
@@ -14,9 +14,40 @@ public class Practice {
         int result = score / 10;
         return result;
     }
+    public String fizzBuzzBasic(int num){
+        String result;
+        if (num % 5 == 0){
+            if (num % 3 ==0){
+                result = "FizzBuzz";
+            } else {
+                result = "Buzz";
+            }
+        } else {
+            if (num % 3 == 0){
+                result = "Fizz";
+            } else {
+                result = "No FizzBuzz";
+            }
+        } return result;
+    }
+    public String fizzBuzzUsage(int num) {
+        StringBuilder result = new StringBuilder("");
+        if (num % 3 == 0){
+            result.append("a");
+        }
+        if (num % 5 == 0){
+            result.append("b");
+        }
+        switch (result.substring(0)){
+            case "": return "No FizzBuzz" ;
+            case "a": return "Fizz";
+            case "b": return "Buzz";
+            default: return "FizzBuzz";
+        }
+    }
     public static void main(String[] args) {
         Practice test1 = new Practice();
-        System.out.println(test1.ControlFlow(35));
+        System.out.println(test1.fizzBuzzUsage(11));
     }
 }
 
